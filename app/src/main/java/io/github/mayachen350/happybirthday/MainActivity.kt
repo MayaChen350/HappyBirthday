ca518db943a1d7b3ae4e53c892eaa49e17342490
Maya <145165822+MayaChen350@users.noreply.github.com>
2024-06-20 00:40:56 -0400

    Init

---
+++ app/src/main/java/io/github/mayachen350/happybirthday/MainActivity.kt
@@ -0,0 +1,70 @@
+package io.github.mayachen350.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import io.github.mayachen350.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HappyBirthdayTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(message = "Happy Birthday!", from = "From Maya")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            textAlign = TextAlign.Center,
+            text = message,
+            fontSize = 90.sp,
+            lineHeight = 100.sp
+        )
+
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String) {
+    
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        GreetingText(message = "Happy Birthday!", from = "From Maya")
+    }
+}
